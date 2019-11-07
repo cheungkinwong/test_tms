ef921f7a70a4c3b98b64ebae40415bf84115d03a
cheungkinwong <cheungkin.wong@gmail.com>
2019-11-07 10:08:14 +0100

    added auto

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>{{ $t('Hello robot and butterfly!') }}, {{$t('Welcome')}}, {{$t('qdfq')}},{{$t('Butterfly')}}</p>
+  <p>{{ $t('Hello robot and butterfly!') }}, {{$t('Welcome')}}, {{$t('qdfq')}},{{$t('Butterfly')}}, {{$t('auto')}}</p>
 </template>
 
 <script>
